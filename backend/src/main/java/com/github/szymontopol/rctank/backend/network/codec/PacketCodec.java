114f665ce742fb5029912a33409f3462d3daac16
SzymonTopol <st316307@student.polsl.pl>
2026-09-15 18:38:33 +0200

    Added encoding of CMD_MOVE

--- backend/src/main/java/com/github/szymontopol/rctank/backend/network/codec/PacketCodec.java
+++ backend/src/main/java/com/github/szymontopol/rctank/backend/network/codec/PacketCodec.java
@@ -16,6 +16,10 @@ public class PacketCodec {
     public static final byte CMD_MOVE = 0x01;
     public static final byte CMD_TELEMETRY = 0x02;
 
+    // 1 byte (ID) + 2 shorts
+    private static final int EXPECTED_CMD_MOVE_SIZE =
+            Byte.BYTES +
+            (2 * Short.BYTES);
     // 1 byte (ID) + 4 shorts + 6 doubles + 1 float
     private static final int EXPECTED_TELEMETRY_SIZE =
             Byte.BYTES +
@@ -24,10 +28,29 @@ public class PacketCodec {
             Float.BYTES;
 
     public byte[] encodeMoveCommand(MoveCommand moveCommand) {
-        return new byte[0];
+
+        if(moveCommand == null) {
+            throw new IllegalArgumentException("MoveCommand cannot be null");
+
+        }
+        if(moveCommand.packetId() != CMD_MOVE) {
+            throw new IllegalArgumentException("Invalid packet ID");
+        }
+
+        ByteBuffer buffer = ByteBuffer.allocate(EXPECTED_CMD_MOVE_SIZE);
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+
+        buffer.put(moveCommand.packetId());
+        buffer.putShort(moveCommand.leftExpectedPower());
+        buffer.putShort(moveCommand.rightExpectedPower());
+        return buffer.array();
     }
 
     public TelemetryData decodeTelemetryData(byte[] payload) {
+        if(payload == null) {
+            System.err.println("Invalid payload. Payload is null");
+            return null;
+        }
 
         if(payload.length < EXPECTED_TELEMETRY_SIZE) {
             System.err.println("Invalid payload length. Size = " + payload.length + " Expected: " + EXPECTED_TELEMETRY_SIZE);
