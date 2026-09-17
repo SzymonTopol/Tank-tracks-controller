6a0089dc10d1bac77b4d0897dc38b665d8bef0a5
SzymonTopol <st316307@student.polsl.pl>
2026-09-17 23:45:01 +0200

    basic gameLoop for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,40 @@
-import { useState } from 'react'
+import { Joystick } from 'react-joystick-component'
+import { useGameLoop } from './hooks/useGameLoop';
+
+const constraint = (val, min, max) => Math.round(Math.max(min, Math.min(max,val)));
+
+const RESOLUTION = 8;
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { updateMotors } = useGameLoop();
+
+  const handleMove = (event) => {
+    console.log("Moved:", event);
+    const{x,y} = event;
+
+    let left = constraint((y+x)*(Math.pow(2,RESOLUTION)-1),-(Math.pow(2,RESOLUTION)-1),Math.pow(2,RESOLUTION)-1);
+    let right= constraint((y-x)*(Math.pow(2,RESOLUTION)-1),-(Math.pow(2,RESOLUTION)-1),Math.pow(2,RESOLUTION)-1);
+
+    updateMotors(left,right);
+  }
+
+  const handleStop = () => {
+    console.log("Stopped");
+    updateMotors(0,0);
+  }
 
   return (
     <>
       <div>TankUI</div>
+      <Joystick
+      size={100}
+      sticky={false}
+      baseColor="#EEEEEE"
+      stickColor="#BBBBBB"
+      move={handleMove}
+      stop={handleStop}
+      />
     </>
   )
 }
--- frontend/src/hooks/useGameLoop.js
+++ frontend/src/hooks/useGameLoop.js
@@ -1,7 +1,31 @@
-import {useRef} from 'react';
+import {useEffect, useRef} from 'react';
 
 export function useGameLoop() {
     const lastTxTime = useRef(0);
+    const INTERVAL = 50;
 
     const motors = useRef({ left: 0, right: 0});
+
+    const requestRef = useRef();
+
+    const updateMotors = (left, right) => {
+        motors.current = {left, right};
+    }
+
+
+    useEffect(() =>{
+        const tick = (currentTime) => {
+
+            if(currentTime > lastTxTime.current + INTERVAL){
+                lastTxTime.current += INTERVAL;
+                console.log("Interval joystic data: ", motors.current);
+            };
+            requestRef.current = requestAnimationFrame(tick);
+        }
+        requestRef.current = requestAnimationFrame(tick);
+
+        return () => cancelAnimationFrame(requestRef.current);
+    }, []);
+
+    return {updateMotors};
 }
